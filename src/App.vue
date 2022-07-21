d2b0953b4fd3a0282f3bdde09629dc7efcbcfa9e
mertbulut <mrtebulute@gmail.com>
2022-07-21 10:50:56 +0300

    f

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import Home from './Home.vue'
+import About from './About.vue'
+import NotFound from './NotFound.vue'
+import Company from './Comp.vue'
+import Navbar from './Navbar.vue'
+const routes = {
+  '/': Home,
+  '/about': About,
+  '/company' : Company
+}
+
+export default {
+  components:{
+    Navbar
+  },
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFound
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+		  this.currentPath = window.location.hash
+		})
+  }
+}
+</script>
+
+<template>
+  <navbar></navbar>
+  <component :is="currentView" />
+</template>
---
+++ src/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+<ul style="list-style: none; display:flex;">
+  <li style="margin-right:5px;"><a href="#/" style="text-decoration: none;">Home</a> </li>
+  <li style="margin-right:5px;"> <a href="#/about" style="text-decoration: none;">About</a> </li>
+  <li style="margin-right:5px;"> <a href="#/company" style="text-decoration: none;">Company</a> </li>
+  <li style="margin-right:5px;"><a href="#/non-existent-path" style="text-decoration: none;">Broken Link</a></li>
+</ul>
+
+ 
+ 
+  
+  
+</template>
